395fd3a7867f8c3dfc4ad63082fcd7749e9a2710
Harikalyan <harikalyan99tronics@gmail.com>
2024-12-25 10:29:13 +0530

    using axios for auth

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -6,10 +6,16 @@ import bcrypt from 'bcrypt';
 export async function POST(request, _) {
     try {
         const {username, email, password} = await request.json();
-        const hashedPassword = await bcrypt.hashSync(password, 10)
+        const hashedPassword = await bcrypt.hashSync(password, 10);
         await connectToMongo();
-        await User.create({username, email, password: hashedPassword});
-        return NextResponse.json({message: "User registered"}, {status: 201})
+        const userExists = await User.findOne({email});
+        if(userExists){
+            return NextResponse.json({error: "User already exists"}, {status: 400});
+        }
+
+        const newUser = new User({username, email, password: hashedPassword});
+        const user = await newUser.save();
+        return NextResponse.json({message: "User registered",success:true, user}, {status: 201})
     } catch (error) {
         return NextResponse.json({message: "An error occured while registering the user"}, {status: 500})
     }
--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -1,69 +1,77 @@
-'use client'
-import Link from '@node_modules/next/link'
-import React, { useRef, useState } from 'react'
+"use client";
+import Link from "@node_modules/next/link";
+import React, { useRef, useState } from "react";
+import axios from "axios";
 
 const RegisterForm = () => {
-    const usernameRef = useRef("");
-    const emailRef = useRef("");
-    const passwordRef = useRef("");
-    const [error, setError] = useState();
+  const usernameRef = useRef("");
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const [error, setError] = useState();
 
-    const handleSubmit = async(e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        const username = usernameRef.current.value;
-        const email = emailRef.current.value;
-        const password = passwordRef.current.value;
+    const username = usernameRef.current.value;
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
 
-        if(!username || !email || !password){
-            setError("All fields mandatory*");
-            return
-        }
-        setError('');
-
-        try {
-            const response = await fetch('api/register', {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    username, email, password
-                })
-            })
-
-            if(response.ok){
-                const form = e.target;
-                form.reset()
-            }else{
-                console.log("User registration failed");
-            }
-        } catch (error) {
-            console.log('Error occured during registration.', error);
-        }
+    if (!username || !email || !password) {
+      setError("All fields mandatory*");
+      return;
     }
+    setError("");
+
+    try {
+      const { data } = await axios.post("api/register", {
+        username,
+        email,
+        password,
+      });
+
+      console.log(data);
+     
+    } catch (error) {
+      console.log("Error occured during registration.", error);
+    }
+  };
   return (
-    <div className='grid place-items-center h-screen'>
-        <div className='shadow-lg p-5 rounded-lg border-t-4 border-green-400'>
-            <h1 className='text-xl font-bold my-4'>Register</h1>
+    <div className="grid place-items-center h-screen">
+      <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
+        <h1 className="text-xl font-bold my-4">Register</h1>
 
-            <form className='flex flex-col gap-3' onSubmit={(e) => handleSubmit(e)}>
-            <input type="text" placeholder='Enter your username' ref={usernameRef} />
-                <input type="email" placeholder='Enter your email' ref={emailRef} />
-                <input type="password" placeholder='Enter your password' ref={passwordRef} />
-                <button className='bg-green-600 text-white font-bold cursor-pointer px-6 py-2' type='submit'>
-                    Login
-                </button>
+        <form className="flex flex-col gap-3" onSubmit={(e) => handleSubmit(e)}>
+          <input
+            type="text"
+            placeholder="Enter your username"
+            ref={usernameRef}
+          />
+          <input type="email" placeholder="Enter your email" ref={emailRef} />
+          <input
+            type="password"
+            placeholder="Enter your password"
+            ref={passwordRef}
+          />
+          <button
+            className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2"
+            type="submit"
+          >
+            Login
+          </button>
 
-                {error && <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">{error}</div>}
+          {error && (
+            <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+              {error}
+            </div>
+          )}
 
-                <Link className='text-sm mt-3 text-right' href={"/"}>
-                Already have an account? <span className='underline'>Login</span>
-                </Link>
-            </form>
-        </div>
+          <Link className="text-sm mt-3 text-right" href={"/"}>
+            Already have an account? <span className="underline">Login</span>
+          </Link>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default RegisterForm
+export default RegisterForm;
