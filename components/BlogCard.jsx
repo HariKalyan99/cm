579f513000ed56817d9cd1cc8c39d2d8c5da0368
Harikalyan99 <harikalyan99tronics@gmail.com>
2025-01-11 11:55:16 +0530

    added the CRUD functionality

--- components/BlogCard.jsx
+++ components/BlogCard.jsx
@@ -6,11 +6,13 @@ import Cookies from "js-cookie";
 import React, { useEffect, useState } from "react";
 import { FaUserAlt } from "react-icons/fa";
 import { RiDeleteBin5Fill } from "react-icons/ri";
-import toast from '@node_modules/react-hot-toast/dist';
-
+import { BiSolidEdit } from "react-icons/bi";
+import toast from "@node_modules/react-hot-toast/dist";
+import BlogEdit from "./BlogEdit";
 
 const BlogCard = ({ blogs, setPostList, postList }) => {
   const [deleteBlogId, setDeleteBlogId] = useState("");
+  const [openEditTab, setOpenEditTab] = useState(false);
 
   useEffect(() => {
     const deleteBlog = async (id) => {
@@ -21,8 +23,8 @@ const BlogCard = ({ blogs, setPostList, postList }) => {
             Authorization: `Bearer ${token}`,
           },
         });
-        if(data.status){
-          setPostList(postList.filter(x => x._id !== id));
+        if (data.status) {
+          setPostList(postList.filter((x) => x._id !== id));
           toast.success(data.message);
         }
       } catch (error) {
@@ -40,32 +42,46 @@ const BlogCard = ({ blogs, setPostList, postList }) => {
   };
 
   return (
-    <div className="min-h-[400px] h-[auto] w-[500px] border border-t-[#838383] border-t-8 border-t-[#FF66C4] border-b-8 shadow hover:shadow-lg flex flex-col justify-evenly p-5 rounded bg-black rounded-xl">
-      <h1 className="text-white text-xl">
-        <span className="font-bold text-[2rem]">Title:</span>
-        {"  "} {blogs.title}
-      </h1>
-      <p className="text-white text-xl">
-        <span className="font-bold text-[2rem]">Description:</span>
-        {"  "} {blogs.content.slice(0, 100)}
-        <Link href={`/blogdetails/${blogs._id}`}>
-          {"  "}
-          <span className="underline hover:text-[#FF66C4] ">Read more...</span>
-        </Link>
-      </p>
-      <div className="flex items-center gap-5 justify-start">
-        <div className="rounded-full border shadow h-10 w-10 flex justify-center items-center">
-          <FaUserAlt className="text-white" />
+    <>
+      {!openEditTab ? (
+        <div className="min-h-[400px] h-[auto] w-[500px] border border-t-[#838383] border-t-8 border-t-[#FF66C4] border-b-8 shadow hover:shadow-lg flex flex-col justify-evenly p-5 rounded bg-black rounded-xl">
+          <h1 className="text-white text-xl">
+            <span className="font-bold text-[2rem]">Title:</span>
+            {"  "} {blogs.title}
+          </h1>
+          <p className="text-white text-xl">
+            <span className="font-bold text-[2rem]">Description:</span>
+            {"  "} {blogs.content.slice(0, 100)}
+            <Link href={`/blogdetails/${blogs._id}`}>
+              {"  "}
+              <span className="underline hover:text-[#FF66C4] ">
+                Read more...
+              </span>
+            </Link>
+          </p>
+          <div className="flex items-center gap-5 justify-start">
+            <div className="rounded-full border shadow h-10 w-10 flex justify-center items-center">
+              <FaUserAlt className="text-white" />
+            </div>
+            <span className="text-white text-xl">
+              <span className="font-bold text-[2rem]">Author:</span>
+              {"  "} {blogs.author}
+            </span>
+          </div>
+          <div className="flex gap-5">
+            <span onClick={() => handleBlogDelete(blogs._id)}>
+              <RiDeleteBin5Fill className="text-[white] text-3xl hover:text-[pink] hover:cursor-pointer" />
+            </span>
+
+            <span onClick={() => setOpenEditTab((prev) => !prev)}>
+              <BiSolidEdit className="text-[white] text-3xl hover:text-[pink] hover:cursor-pointer" />
+            </span>
+          </div>
         </div>
-        <span className="text-white text-xl">
-          <span className="font-bold text-[2rem]">Author:</span>
-          {"  "} {blogs.author}
-        </span>
-      </div>
-      <span onClick={() => handleBlogDelete(blogs._id)}>
-        <RiDeleteBin5Fill className="text-[white] text-3xl hover:text-[pink] hover:cursor-pointer" />
-      </span>
-    </div>
+      ) : (
+        <BlogEdit setOpenEditTab={setOpenEditTab} setPostList={setPostList} postList={postList} blogs={blogs}/>
+      )}
+    </>
   );
 };
 
---
+++ components/BlogEdit.jsx
@@ -0,0 +1,82 @@
+"use client";
+
+import axios from "@node_modules/axios";
+import Cookies from "js-cookie";
+import React, { useEffect, useState } from "react";
+import { MdOutlineEditOff } from "react-icons/md";
+
+const BlogEdit = ({ setOpenEditTab, blogs, postList, setPostList }) => {
+  const [blogTitle, setBlogTitle] = useState(blogs.title);
+  const [blogContent, setBlogContent] = useState(blogs.content);
+
+  const [editBlog, setEditBlog] = useState("");
+
+  useEffect(() => {
+    const updateBlog = async(blog, id) => {
+        try {
+            const token = Cookies.get('jwt');
+            const {data} = await axios.put(`/api/blogs/${id}`, blog,  {
+                headers: {
+                    'Authorization': `Bearer ${token}`,  
+                    'Content-Type': 'application/json'
+                }
+            });
+            const index = postList.findIndex(x => x._id === data.blog._id);
+            postList.splice(index,1,data.blog);
+            setPostList([...postList]);
+            setOpenEditTab(prev => !prev);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    if(editBlog?.title){
+        updateBlog(editBlog, blogs._id)
+    }
+  }, [editBlog])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const title = blogTitle;
+    const content = blogContent;
+    setEditBlog({title, content});
+  }
+
+
+  return (
+    <div>
+      <h1 className="text-xl font-bold my-4">You can edit your blog here!</h1>
+
+      <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Title of your blog"
+          required
+          className="w-[400px]"
+          value={blogTitle}
+          onChange={(e) => setBlogTitle(e.target.value)}
+        />
+        <textarea
+          type="text"
+          placeholder="Description of your blog"
+          required
+          rows={10}
+          className="w-[400px]"
+          value={blogContent}
+          onChange={(e) => setBlogContent(e.target.value)}
+        />
+        <button
+          className="bg-[black] text-white font-bold cursor-pointer px-6 py-2 hover:bg-[grey]"
+          type="submit"
+        >
+          Edit Blog
+        </button>
+        <span onClick={() => setOpenEditTab((prev) => !prev)}>
+          <MdOutlineEditOff className="text-[black] text-3xl hover:text-[grey] hover:cursor-pointer" />
+        </span>
+      </form>
+    </div>
+  );
+};
+
+export default BlogEdit;
